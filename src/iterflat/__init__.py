829847014ddfed82d8b9f642df907b47fec7a169
Johannes <johannes.programming@gmail.com>
2025-10-30 00:41:08 +0100

    Version 0.1.0a0

--- src/iterflat/__init__.py
+++ src/iterflat/__init__.py
@@ -1,5 +1,2 @@
 from iterflat.core import *
 from iterflat.tests import *
-
-if __name__ == "__main__":
-    main()
--- src/iterflat/core/__init__.py
+++ src/iterflat/core/__init__.py
@@ -1,8 +1,21 @@
+import operator
 from typing import *
 
-__all__ = ["main"]
+__all__ = ["iterflat"]
 
 
-def main(args: Optional[Iterable] = None) -> None:
-    "This function prints 'Hello World!'."
-    print("Hello World!")
+def iterflat(data: Any, *, depth: SupportsIndex = 1) -> Generator[Any, None, None]:
+    n: int
+    x: Iterable
+    n = operator.index(depth)
+    if n < -1:
+        yield iterflat(data, depth=n + 1)
+        return
+    if n == -1:
+        yield data
+        return
+    if n == 0:
+        yield from data
+        return
+    for x in data:
+        yield from iterflat(x, depth=n - 1)

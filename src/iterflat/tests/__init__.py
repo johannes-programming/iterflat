492b89abedd4f394585718d0fe328c808137a0a9
Johannes <johannes.programming@gmail.com>
2026-06-20 11:21:19 +0200

    Version 1.0.1

--- src/iterflat/tests/__init__.py
+++ src/iterflat/tests/__init__.py
@@ -3,14 +3,10 @@ import unittest
 __all__ = ["test"]
 
 
-def test() -> unittest.TextTestRunner:
+def test() -> unittest.TextTestResult:
     "This function runs all the tests."
     loader: unittest.TestLoader
     tests: unittest.TestSuite
-    runner: unittest.TextTestRunner
-    result: unittest.TextTestResult
     loader = unittest.TestLoader()
     tests = loader.discover(start_dir="iterflat.tests")
-    runner = unittest.TextTestRunner()
-    result = runner.run(tests)
-    return result
+    return unittest.TextTestRunner().run(tests)
